646811a176c5b7866efbd59d8c0067e691ff48d7
winnerwly <975080391@qq.com>
2018-05-24 15:52:50 +0800

    修改推广管理

--- src/services/qr.js
+++ src/services/qr.js
@@ -1,6 +1,15 @@
-import { request, config, pageParams } from '../utils'
+import { request, config } from '../utils'
 
 const { qr } = config.api
+const pageParams = function (params) {
+  params = { ...params } || {
+    pagination: 1,
+    rownum: 10,
+  }
+  params.pagination = params.page || 1
+  params.rownum = params.pageSize || 10
+  return params
+}
 
 export async function querydetail (params) {
   return request({
