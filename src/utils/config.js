a7223a30eb6859b6bd257fdb7999dc70a6165181
winnerwly <975080391@qq.com>
2018-07-31 14:48:40 +0800

    ....

--- src/utils/config.js
+++ src/utils/config.js
@@ -19,13 +19,12 @@ APIV3 = 'http://app.quandikeji.com:8288'
 // 张涛
 // APIV3 = 'http://106.14.212.146:8288'
 APIV3 = 'http://192.168.231.239:8066'
-// APIV3 = 'http://192.168.231.239:8077'
+
+// 山东服务器
 // APIV3 = 'http://117.50.23.22:8080'
 
 // 生产环境时api固定为线上url
 if (process.env.NODE_ENV !== 'development') {
-  // APIV3 = 'http://app.quandikeji.com:8288'
-  // APIV3 = 'http://47.101.42.216:8088'
   APIV3 = 'http://106.14.212.146:8288'
 }
 
