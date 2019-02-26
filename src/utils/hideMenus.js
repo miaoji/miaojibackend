1b5e8af16a193bb8583b79e79250c3a9f2ddb6e0
winnerwly <975080391@qq.com>
2019-02-26 11:02:38 +0800

    ...

--- src/utils/hideMenus.js
+++ src/utils/hideMenus.js
@@ -1,18 +1,5 @@
 export default [
   {
-    id: '1111',
-    mpid: '1',
-    name: '接口对接',
-    icon: 'message',
-    route: '/docking',
-  }, {
-    id: '1112',
-    mpid: '-1',
-    name: '接口对接明细',
-    icon: 'message',
-    route: '/dockingdetail',
-  },
-  {
     id: '1001',
     mpid: '-1',
     icon: 'laptop',
@@ -85,5 +72,12 @@ export default [
     mpid: '-1',
     name: '分派人详情',
     route: '/assignor',
+  },
+  {
+    id: '1013',
+    mpid: '-1',
+    name: '接口对接明细',
+    icon: 'message',
+    route: '/dockingdetail',
   },
 ]
